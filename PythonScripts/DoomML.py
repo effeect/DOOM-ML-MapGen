d036a8fe55778cc8817c84c35d5286fa47c9ca5c
Oliver Dimes <oliver.dimes3@gmail.com>
2020-01-28 17:39:46 +0000

    Vertex Model works and outputs data
    However, there needs to be a rework of that model. However the importing and exporting is functional and working

--- PythonScripts/DoomML.py
+++ PythonScripts/DoomML.py
@@ -48,149 +48,48 @@ sidedefs = json_normalize(d['sidedefs'])
 vertices = json_normalize(d['vertices'])
 sectors = json_normalize(d['sectors'])
 
-vertices.plot(x='x',y='y', kind='scatter')
+#Vertex Functions NEEDS A REWORKED MODEL
+def vertex():
+    #Making a copy of the Vertex Dataset
 
-print(vertices)
-
-#I'm choosing Vertex Model first since it only contains two features
-def vertexModel(file):
-    vertexDataset = vertices.copy()
-
-    #These two lines seperate the data into two seperate components
-    train = vertexDataset.sample(frac=0.8, random_state=0)
-    test = vertexDataset.drop(train.index)
-
-    train_labels = train.pop("x")
-    test_labels = test.pop("x")
-
-    trainStats = train.describe()
-    trainStats = train.transpose()
-
-    # Normalization function for data
-    # def norm(x):
-    #     return (x - trainStats['mean']) / trainStats['std']
-    #
-    # normed_train_data = norm(train)
-    # normed_test_data = norm(test)
-
-    def build_model():
-        model = keras.Sequential([
-            layers.Dense(64, activation='relu', input_shape=[len(train.keys())]),
-            layers.Dense(64, activation='relu'),
-            layers.Dense(1)
-        ])
-
-        optimizer = tf.keras.optimizers.RMSprop(0.001)
-
-        model.compile(loss='mse',
-                      optimizer=optimizer,
-                      metrics=['mae', 'mse'])
-        return model
-
-    model = build_model()
-    model.summary()
-
-    example_batch = train[:10]
-    example_result = model.predict(example_batch)
-
-    EPOCHS = 1000
-    history = model.fit(
-        train, train_labels,
-        epochs=EPOCHS, validation_split=0.2, verbose=0)
-
-    hist = pd.DataFrame(history.history)
-    hist['epoch'] = history.epoch
-    hist.tail()
-
-#Another method
-def vertexModelTwo():
     df = vertices.copy()
-
-    #Visual display of scattergram
-    df.plot(x='x',y='y', kind='scatter')
-
-
     # Thanks for https://stackoverflow.com/questions/42286972/converting-from-pandas-dataframe-to-tensorflow-tensor-object
 
     # Splitting up the vertex coords
     xTensor = tf.constant(df['x'])
     yTensor = tf.constant(df['y'])
-    print(xTensor)
-    print(type(xTensor))
     #Creating Dataset
+
     dataset = tf.data.Dataset.from_tensor_slices((xTensor, yTensor))
 
-
-
-
-    print(type(dataset))
     train_dataset = dataset.shuffle(300).batch(64)
-    print(dataset)
-
-    # https://www.tensorflow.org/datasets/add_dataset
 
     # Model Function
     def get_compiled_model():
         #Model for the function
         model = keras.Sequential([
-            layers.Dense(64, activation='relu'),
-            layers.Dense(64, activation='relu'),
-            layers.Dense(1)
+            layers.Dense(256, activation='relu'),
+            layers.Dense(256, activation='relu'),
+            layers.Dense(2)
         ])
 
-        optimizer = tf.keras.optimizers.Adagrad(0.001)
+        optimizer = tf.keras.optimizers.Adam(0.01)
 
-        model.compile(loss='mse',
+        model.compile(loss='mean_squared_error',
                       optimizer=optimizer,
-                      metrics=['mae', 'mse'])
+                      metrics=['accuracy'])
 
         return model
 
-    # https://www.tensorflow.org/guide/keras/rnn
-    def model_generation():
-        model = tf.keras.Sequential()
-        model.add(layers.Embedding(input_dim=1000, output_dim=64))
-
-        # The output of GRU will be a 3D tensor of shape (batch_size, timesteps, 256)
-        model.add(layers.GRU(256, return_sequences=True))
-
-        # The output of SimpleRNN will be a 2D tensor of shape (batch_size, 128)
-        model.add(layers.SimpleRNN(128))
-
-        model.add(layers.Dense(10, activation='softmax'))
-
-        model.summary()
-
-    def estimator():
-        #Building a baseline regressor
-        regressor = tf.estimator.BaselineRegressor()
-
-        print(type(train_dataset))
-
-
-        #Defining feature columns
-        x = tf.feature_column.numeric_column('x')
-        y = tf.feature_column.numeric_column('y')
-
-
-
-    estimator()
-
-
-
-
-
-    # model_generation()
     model = get_compiled_model()
 
-    model.fit(train_dataset, epochs=100)
-    NewStuff = model.predict(train_dataset).flatten()
+    model.fit(train_dataset, epochs=1000)
+    numpyArray = model.predict(train_dataset)
 
-    #Gives us the new data
-    print(NewStuff)
-
-def things():
-    number = 4
+    result = pd.DataFrame(numpyArray)
+    print(result)
+    result.plot(x=0,y=1, kind='scatter')
+    plt.show()
 
 def linedefs() :
     number = 3
@@ -201,7 +100,7 @@ def sidedefs() :
 def sectors() :
     bumner = 1
 
-vertexModelTwo()
+vertex()
 
 def predictPoints(model, data):
     # https://stackoverflow.com/questions/42766458/tensorflow-predicting-sequences-what-is-x-and-y
