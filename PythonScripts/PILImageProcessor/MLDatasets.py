6c8c27c4272af78f5e7dea22b84d47de5677368e
Oliver Dimes <oliver.dimes3@gmail.com>
2020-05-11 12:51:17 +0100

    Tensorflow addition (Almost Finished)
    The next step is to implement LineDef functionality. This may be slightly tricky but a simple approach to it will be fine.

--- PythonScripts/PILImageProcessor/MLDatasets.py
+++ PythonScripts/PILImageProcessor/MLDatasets.py
@@ -1,5 +1,8 @@
 # TensorFlow and tf.keras
 import tensorflow as tf
+import pandas as pd
+import os
+import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
 
@@ -7,14 +10,59 @@ from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
 
-def trainVertexPoints() :
+#Printing out essential details to help debugging
+print("TensorFlow version : {}".format(tf.__version__))
+print("Eager execution : {}".format(tf.executing_eagerly()))
 
-    # https://datascience.stackexchange.com/questions/16890/neural-network-for-multiple-output-regression
+#Using two linear regression algorithms to get the correct result
+# https://www.geeksforgeeks.org/linear-regression-using-tensorflow/
+
+#Global Data Lists for Application
+pointX = []
+pointY = []
+iPointX = []
+iPointY = []
+
+#This function processes the data from
+def prepareData( filename ) :
+    #Reading file
+    dfTemp = pd.read_csv(filename)
+    df = dfTemp.copy()
+    df.head()
+
+    #Popping each individual mark
+    imagePoints = df.pop('imagePoint')
+    points = df.pop('point')
+
+    #Making Copies
+    npImagePoints = imagePoints
+    npPoints = points
+
+    #NewDataPoints
+
+    #String issue with data, this isn't the best way to solve the problem but it's a minimal cost in the grand scheme of things
+    for i in npPoints:
+        tempPointX = float(i.split(',',1)[0].replace('[',''))
+        tempPointY = float(i.split(',',1)[1].replace(']',''))
+        pointX.append(tempPointX)
+        pointY.append(tempPointX)
+
+    for i in npImagePoints:
+        tempImagePointX = float(i.split(',',1)[0].replace('[',''))
+        tempImagePointY = float(i.split(',',1)[1].replace(']',''))
+        iPointX.append(tempImagePointX)
+        iPointY.append(tempImagePointY)
+
+
+
+prepareData("CATWALK.csv")
+
+def trainVertexPoints() :
     def build_model():
         model = keras.Sequential([
             layers.Dense(64, activation='relu'),
             layers.Dense(64, activation='relu'),
-            layers.Dense(2, activation='linear')
+            layers.Dense(1)
         ])
 
         optimizer = tf.keras.optimizers.RMSprop(0.001)
@@ -23,3 +71,24 @@ def trainVertexPoints() :
                       optimizer=optimizer,
                       metrics=['mae', 'mse'])
         return model
+
+    EPOCHS = 1000
+
+    model = build_model()
+
+    # Using TF.Keras to normalise data arrays
+    tfiPointX = np.asarray(iPointX)
+    tfPointX = np.asarray(pointX)
+    tfiPointY = tf.keras.utils.normalize(iPointY)
+    tfPointY = tf.keras.utils.normalize(pointY)
+
+    history = model.fit( tfiPointX , tfPointX, epochs=EPOCHS)
+    results = model.predict(tfiPointX,batch_size=32)
+
+    print(results)
+
+
+
+
+
+trainVertexPoints()
